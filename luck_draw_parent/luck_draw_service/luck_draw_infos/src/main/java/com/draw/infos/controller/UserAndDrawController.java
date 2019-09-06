645bb2861be7f4a012f4d91b0151ba24f45695e6
guojuan <954783056@qq.com>
2019-09-06 22:01:28 +0800

    open

---
+++ luck_draw_parent/luck_draw_service/luck_draw_infos/src/main/java/com/draw/infos/controller/UserAndDrawController.java
@@ -0,0 +1,24 @@
+package com.draw.infos.controller;
+
+
+import com.draw.entity.Result;
+import com.draw.entity.StatusCode;
+import com.draw.infos.service.UserAndDrawService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userAndDraw")
+public class UserAndDrawController {
+
+    @Autowired
+    private UserAndDrawService  userAndDrawService;
+
+    @RequestMapping("userAndDraw/findUid")
+    public Result findUid(@RequestParam("code")Long code, @RequestParam("did")String  did){
+        String  uid=userAndDrawService.findUid(code, did);
+        return new Result(true, StatusCode.OK,"查询成功",uid);
+    }
+}
